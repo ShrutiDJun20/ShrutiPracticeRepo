fa7ccb0e00f8881d3c17d84813fb4be290e68b8d
Shruti_Dubey <shrutidubey69@gmail.com>
2020-08-08 01:38:47 +0530

    changed done on 1002

--- src/package2/Class1.java
+++ src/package2/Class1.java
@@ -4,6 +4,8 @@ public class Class1 {
 
 	void main(){
 	System.out.println("Test");
+	System.out.println("changes done on branch 1002-1");
+	System.out.println("changes done on branch 1002-2");
 	}
 	
 }
