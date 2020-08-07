3fce0ca56c37558a4bb9d0b73b52e7b2cea754b8
Shruti_Dubey <shrutidubey69@gmail.com>
2020-08-07 22:40:23 +0530

    comit test2test3

---
+++ src/package1/Test2.java
@@ -0,0 +1,13 @@
+package package1;
+
+public class Test2 {
+	void m1(){
+		System.out.println("in m1 of test2");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("in main of test2");
+		System.out.println("in main of test2");
+		System.out.println("in second banch test 2");
+	}
+}
---
+++ src/package1/Test3.java
@@ -0,0 +1,8 @@
+package package1;
+
+public class Test3 {
+ public static void main(String[] args) {
+	System.out.println("in main test 3");
+	System.out.println("in second banch test 3");
+}
+}
